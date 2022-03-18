84c948d958583491c190b61db4e8f43d774904ef
Pavel <pasha1201s@inbox.ru>
2022-03-18 20:56:19 +0300

    stok

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import numpy as np
 from PIL import Image
 from OpenGL.GL.shaders import *
 
-import sys
+import sys, os
 
 global img
 global img_arr
@@ -15,7 +15,7 @@ global Height
 global vertexVBO
 vertexVBO = glGenFramebuffers(1)
 
-img = Image.open('2.jpg').rotate(0)
+img = Image.open(os.path.abspath(os.curdir)+'/imgs/2.jpg').rotate(0)
 img_arr = np.array(
     img.getdata(),
     np.uint8
@@ -25,45 +25,45 @@ Width, Height = img.size
     
 def load_shaders():
     global vertex
-    with open('vertex.glsl', 'r') as f:
+    with open('glsl/vertex.glsl', 'r') as f:
         vertex = f.read()
 
     global fragment
-    with open('fragmentRGB2YIQ.glsl', 'r') as f:
+    with open('glsl/fragmentRGB2YIQ.glsl', 'r') as f:
         fragment = f.read()
 
-    global fragment1
-    with open('fragment.glsl', 'r') as f:
-        fragment1 = f.read()
+    # global fragment1
+    # with open('glsl/fragment.glsl', 'r') as f:
+    #     fragment1 = f.read()
 
     vertex = ''.join(vertex)
     fragment = ''.join(fragment)
-    fragment1 = ''.join(fragment1)
+    # fragment1 = ''.join(fragment1)
 
 
 def init():
     # createBuffer
-    glBindFramebuffer(GL_FRAMEBUFFER, vertexVBO)
+    # glBindFramebuffer(GL_FRAMEBUFFER, vertexVBO)
 
-    texture = glGenTextures(1)
-    glBindTexture(GL_TEXTURE_2D, texture)
-    glTexImage2D(
-        GL_TEXTURE_2D,
-        0,
-        GL_RGB,
-        Width,
-        Height,
-        0,
-        GL_RGBA,
-        GL_UNSIGNED_BYTE,
-        None
-    )
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST )
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST )
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE )
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE )
+    # texture = glGenTextures(1)
+    # glBindTexture(GL_TEXTURE_2D, texture)
+    # glTexImage2D(
+    #     GL_TEXTURE_2D,
+    #     0,
+    #     GL_RGB,
+    #     Width,
+    #     Height,
+    #     0,
+    #     GL_RGBA,
+    #     GL_UNSIGNED_BYTE,
+    #     None
+    # )
+    # glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST )
+    # glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST )
+    # glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE )
+    # glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE )
 
-    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texture, 0)
+    # glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texture, 0)
 
     # endBuffer
     glClearColor(0.7, 1.0, 0.7, 1)
@@ -94,12 +94,12 @@ def init():
 
     vertex_shader = compileShader(vertex, GL_VERTEX_SHADER)
     fragment_shader = compileShader(fragment, GL_FRAGMENT_SHADER)
-    fragment_shader1 = compileShader(fragment1, GL_FRAGMENT_SHADER)
+    # fragment_shader1 = compileShader(fragment1, GL_FRAGMENT_SHADER)
 
     global program
     program = compileProgram(vertex_shader, fragment_shader)
-    global program1
-    program1 = compileProgram(vertex_shader, fragment_shader1)
+    # global program1
+    # program1 = compileProgram(vertex_shader, fragment_shader1)
 
     
 
@@ -115,7 +115,7 @@ def draw():
     # Load OpenGL(with shaders)program context 
     # glBindBuffer(GL_TEXTURE_2D, )
     glUseProgram(program)
-    glUseProgram(program1)
+    # glUseProgram(program1)
 
     #Texture                
     
